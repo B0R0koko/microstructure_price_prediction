099ab7066eba4bd2b6d167478a080b00a233a244
LeatherBag011235 <93046087+LeatherBag011235@users.noreply.github.com>
2024-12-18 20:37:03 +0300

    Scaling feature logic added, pemutation feature importance added

--- src/data_analysis/processing_utils.py
+++ src/data_analysis/processing_utils.py
@@ -13,7 +13,47 @@ class DataPrepare():
         self.df_train = None
         self.df_test = None
 
-    def train_test_split(self, train_test_ratio: float = 0.8, print_stat: bool=True, cols_to_exclude: list[str]=None) -> tuple[pl.DataFrame, pl.DataFrame]:
+    def normalize(self, cross_section_id: str, exclude_columns: list[str] = None) -> pl.DataFrame:
+        """
+        Normalize the DataFrame by groups defined in `cross_section_id`.
+        
+        Parameters:
+        - cross_section_id (str): Column used to define groups for normalization.
+        - exclude_columns (list[str]): List of columns to exclude from normalization.
+        
+        Returns:
+        - pl.DataFrame: The normalized DataFrame with columns replaced.
+        """
+        if exclude_columns is None:
+            exclude_columns = []
+
+        # Exclude specified columns and cross_section_id
+        features = [col for col in self.df.columns if col not in [cross_section_id] + exclude_columns]
+
+        # Calculate mean and std for each feature grouped by `cross_section_id`
+        grouped = self.df.group_by(cross_section_id).agg([
+            pl.col(column).mean().alias(f"{column}_mean") for column in features
+        ] + [
+            # Use a conditional expression to replace zero std with 1
+            pl.when(pl.col(column).std() == 0)
+            .then(1.0)
+            .otherwise(pl.col(column).std())
+            .alias(f"{column}_std") for column in features
+        ])
+        
+        df_normalized = self.df.join(grouped, on=cross_section_id)
+
+        normalized_columns = [
+            ((pl.col(column) - pl.col(f"{column}_mean")) / pl.col(f"{column}_std")).alias(column)
+            for column in features
+        ]
+        df_normalized = df_normalized.with_columns(normalized_columns)
+
+        self.df = df_normalized.select(self.df.columns)
+
+        return self.df
+
+    def train_test_split(self, train_test_ratio: float = 0.8, print_stat: bool=True, exclude_columns: list[str]=None) -> tuple[pl.DataFrame, pl.DataFrame]:
         """
         Splits the dataset into training and testing sets based on a train-test ratio.
         """
@@ -27,8 +67,8 @@ class DataPrepare():
 
         df = self.df
 
-        if not cols_to_exclude is None:
-            df = df.select(pl.exclude(cols_to_exclude))
+        if not exclude_columns is None:
+            df = df.select(pl.exclude(exclude_columns))
 
         for currency_pair in df["currency_pair"].unique():
 
@@ -91,13 +131,14 @@ class DataPrepare():
         if target_var not in df_train.columns or target_var not in df_test.columns:
             raise ValueError(f"Target variable '{target_var}' not found in the dataset.")
 
-        df_train = df_train.drop_nulls()
+        df_train = df_train.fill_null(strategy="zero")
         X_train = df_train.select(pl.exclude(target_var))
         y_train = df_train.select(pl.col(target_var)).to_numpy().ravel()
 
-        df_test = df_test.drop_nulls()
+        df_test = df_test.fill_null(strategy="zero")
         X_test = df_test.select(pl.exclude(target_var))
         y_test = df_test.select(pl.col(target_var)).to_numpy().ravel()
+        
         if to_pandas == True:
             return X_train.to_pandas(), y_train, X_test.to_pandas(), y_test
 
@@ -107,18 +148,18 @@ class DataPrepare():
         """
         Visualize selected variabals againts trade time in chosen time range, all variables ploted on the same plot.
         """
-        symbols: list[str] = self.df.select(pl.col('symbol')).unique()
+        symbols: list[str] = self.df.select(pl.col('currency_pair')).unique()
 
-        df_for_plot: pl.DataFrame = self.df.filter((pl.col('symbol') == symbols[0]) &
-                                              (pl.col('trade_time').is_between(lower_bound=start_time, upper_bound=end_time)))
+        df_for_plot: pl.DataFrame = self.df.filter((pl.col('currency_pair') == symbols[0]) &
+                                              (pl.col('cross_section_id').is_between(lower_bound=start_time, upper_bound=end_time)))
 
         # Plot all variables on the same plot
         plt.figure(figsize=(12, 8))
 
         for var in variables_to_plot:
-            plt.plot(df_for_plot['trade_time'], df_for_plot[var], label=var)
+            plt.plot(df_for_plot['cross_section_id'], df_for_plot[var], label=var)
 
-        plt.xlabel('Trade Time')
+        plt.xlabel('Cross-section id')
         plt.ylabel('Values')
         plt.title('Variables over Trade Time')
         plt.legend()
