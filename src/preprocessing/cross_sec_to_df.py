a12ea2e40d493f8b37a8ec36849e33f9e9af047e
LeatherBag011235 <93046087+LeatherBag011235@users.noreply.github.com>
2024-12-18 16:40:25 +0300

    Update cross_sec_to_df.py

--- src/preprocessing/cross_sec_to_df.py
+++ src/preprocessing/cross_sec_to_df.py
@@ -4,14 +4,31 @@ sys.path.append(r"C:\Users\310\Desktop\Progects_Py\microstructure_price_predicti
 from datetime import datetime, timedelta
 from pathlib import Path
 import polars as pl
+import time
 
 from core.time_utils import Bounds
 from preprocessing.microstructure_pipeline import MicrostructurePipeline
+from stacking_utils import time_to_end, insert_crossection_id
 
 def main():
+    """
+    Args:
+        hive_dir: Path - directory from data is taken
+        save_dir: Path - directory where dataframe will be saved after execution
+        save: Bool - flag, weather you want to save file or not
+        start_of period: datetime - the begining of time period concidered for dataframe creation
+        end_of_period: datetime - the end ot time period concidered for dataframe creation
+        delta: time_delta - the length of a window that used to calculate one cross-section
+    Overview:
+        Overall this function calls MicrostructurePipeline and calculate crossections. 
+        After cross-section is calculeted it is stacked with another ones. 
+        When all cross-sections created and stacked the function can save the resulting df in spesified directory.
+    """
+
+
     hive_dir: Path = Path(r"C:\Users\310\Desktop\Progects_Py\data\microstructure_price_prediction_data\unzipped")
     save_dir: Path = Path(r"C:\Users\310\Desktop\Progects_Py\data\microstructure_price_prediction_data\dfs")
-    save = True
+    save: bool = True
     start_of_period: datetime = datetime(2024, 6, 15,)
     end_of_period: datetime = datetime(2024, 7, 15,)
     delta: timedelta = timedelta(seconds=10 )
@@ -19,6 +36,9 @@ def main():
 
     bounds: Bounds = Bounds(start_inclusive=start_of_period, end_exclusive=start_of_period + delta)
     staked_df = pl.DataFrame()
+
+    # Record the start time
+    start_time = time.time()
 
     while bounds.end_exclusive <= end_of_period:
         # Load data, compute features and get cross-setion
@@ -35,18 +55,27 @@ def main():
         cross_section_id = bounds.end_exclusive
         
         # Create a series of IDs and insert it in cross-section
-        id_series = pl.Series("cross_section_id" , [cross_section_id] * df_cross_section.shape[0])
-        df_cross_section.insert_column(1, id_series)
-        
+        df_cross_section = insert_crossection_id(cross_section_id=cross_section_id, df_cross_section=df_cross_section)
+
+        # Stack dfs together
+        staked_df = staked_df.vstack(df_cross_section) if not staked_df.is_empty() else df_cross_section
+
         bounds.start_inclusive += delta
         bounds.end_exclusive += delta
 
-        staked_df = staked_df.vstack(df_cross_section) if not staked_df.is_empty() else df_cross_section
+        # Keep track of how much time have passed
+        elapsed_time = time.time() - start_time
+        time_to_end(start_of_period=start_of_period,
+                    end_of_period=end_of_period,
+                    delta=delta,
+                    current_end_bound=bounds.end_exclusive,
+                    elapsed_time=elapsed_time)
 
     print(staked_df)
 
     if save:
         #Spesify file name 
+
         file_name = f"{start_of_period}_{end_of_period}_delta_{delta}_return_5_sec.parquet"
 
         file_path = save_dir / file_name.replace(":", "-")
